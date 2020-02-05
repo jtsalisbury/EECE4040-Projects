8adc1e936bad66ed102448b85d787b82d870741a
JT Salisbury <crazyscouter1@gmail.com>
2020-02-05 12:05:58 -0500

    Add working menu stuff

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -1,6 +1,6 @@
 // HW2.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <array>
 #include <iostream>
 #include <string>
 #include "sorting.h"
@@ -16,75 +16,64 @@ int main() {
     string strList;
 
     while (true) {
+        system("cls");
         mode = 'n';
 
         cout << "This program allows you to use compare mergesort and quicksort." << endl
              << "You will be asked to provide a threshold value and the size of a list to compare. If the size is greater than 15, the list will be auto randomized." << endl
              << "If the size is less than or equal to 15, you can specify to enter in your own values, or to have the list randomized." << endl
-             << "If the size is less than or equal to 15, you may opt to display both the sorted and unsorted lists." << endl << endl;
+             << "If the size is less than or equal to 15, you may opt to display both the sorted and unsorted lists." << endl;
 
         cout << "Please enter the threshold value: ";
         cin >> threshold;
-        cout << endl << "Please enter the list size: ";
+        cout << "Please enter the list size: ";
         cin >> size;
 
         if (size <= 15) {
-            cout << endl << "Would you like to manually enter this list (y = manual, n = randomize this list)? ";
+            cout << "Would you like to manually enter this list (y = manual, n = randomize this list)? ";
             cin >> mode;
 
-            cout << endl << "Would you like to display both the sorted and unsorted lists (y/n)? ";
+            cout << "Would you like to display both the sorted and unsorted lists (y/n)? ";
             cin >> display;
         }
 
-        int* arr = new int[size];
+        cout << endl;
+
+        int* unsortedArr = new int[size];
         if (mode == 'n') {
             for (int i = 0; i < size; i++) {
-                arr[i] = (rand() % 1000) + 1; //random number between 1 and 1000
+                unsortedArr[i] = (rand() % 1000) + 1; //random number between 1 and 1000
             }
         }
         else {
-            bool invalid = true;
+            int arrInd = 0;
+            int num;
 
-            while (invalid) {
-                int spaceCount = 0;
-                invalid = false;
+            while (true) {
+                cout << "Please enter the " << (arrInd + 1) << " list value: ";
+                cin >> num;
 
-                cout << endl << "Please enter in the list values separated by single spaces: ";
-                cin >> strList;
+                unsortedArr[arrInd] = num;
+                arrInd++;
 
-                // Quick and dirty validation based on space count
-                for (auto letter : strList) {
-                    if (letter == ' ') {
-                        spaceCount++;
-                    }
-                }
-
-                // Too many spaces. Don't want to deal with leading or trailing spaces/ extra spaces between words
-                if (spaceCount >= size - 1) {
-                    invalid = true;
-                    cout << endl << "There are too many spaces in this string, please re-enter the list." << endl;
-                }
-                if (spaceCount < size - 1) {
-                    invalid = true;
-                    cout << endl << "There aren't enough numbers for the size of " << size << ", please re-enter the list." << endl;
-                }
-
-                string word;
-                int listInd = 0;
-
-                for (auto letter : strList) {
-                    if (letter == ' ') {
-                        arr[listInd] = stoi(word);
-                        word = "";
-                        listInd = listInd + 1;
-                    }
-                    else {
-                        word += letter;
-                    }
+                if (arrInd >= size) {
+                    break;
                 }
             }
         }
 
-        delete[] arr;
+        int* mergeSortArr = new int[size];
+        int* quickSortArr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            mergeSortArr[i] = unsortedArr[i];
+            quickSortArr[i] = unsortedArr[i];
+        }
+
+        // todo 
+
+        delete[] unsortedArr;
+        delete[] mergeSortArr;
+        delete[] quickSortArr;
     }
 }
