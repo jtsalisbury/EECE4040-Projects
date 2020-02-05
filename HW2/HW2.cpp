1473cce91847048d34de2c15fe42bf7455244ab9
JT Salisbury <crazyscouter1@gmail.com>
2020-02-05 11:21:38 -0500

    Add some menu stuff

--- HW2/HW2.cpp
+++ HW2/HW2.cpp
@@ -2,20 +2,89 @@
 //
 
 #include <iostream>
+#include <string>
 #include "sorting.h"
+#include <time.h>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main() {
+    srand((unsigned)time(0));
+
+    int threshold, size;
+    char mode, display;
+    string strList;
+
+    while (true) {
+        mode = 'n';
+
+        cout << "This program allows you to use compare mergesort and quicksort." << endl
+             << "You will be asked to provide a threshold value and the size of a list to compare. If the size is greater than 15, the list will be auto randomized." << endl
+             << "If the size is less than or equal to 15, you can specify to enter in your own values, or to have the list randomized." << endl
+             << "If the size is less than or equal to 15, you may opt to display both the sorted and unsorted lists." << endl << endl;
+
+        cout << "Please enter the threshold value: ";
+        cin >> threshold;
+        cout << endl << "Please enter the list size: ";
+        cin >> size;
+
+        if (size <= 15) {
+            cout << endl << "Would you like to manually enter this list (y = manual, n = randomize this list)? ";
+            cin >> mode;
+
+            cout << endl << "Would you like to display both the sorted and unsorted lists (y/n)? ";
+            cin >> display;
+        }
+
+        int* arr = new int[size];
+        if (mode == 'n') {
+            for (int i = 0; i < size; i++) {
+                arr[i] = (rand() % 1000) + 1; //random number between 1 and 1000
+            }
+        }
+        else {
+            bool invalid = true;
+
+            while (invalid) {
+                int spaceCount = 0;
+                invalid = false;
+
+                cout << endl << "Please enter in the list values separated by single spaces: ";
+                cin >> strList;
+
+                // Quick and dirty validation based on space count
+                for (auto letter : strList) {
+                    if (letter == ' ') {
+                        spaceCount++;
+                    }
+                }
+
+                // Too many spaces. Don't want to deal with leading or trailing spaces/ extra spaces between words
+                if (spaceCount >= size - 1) {
+                    invalid = true;
+                    cout << endl << "There are too many spaces in this string, please re-enter the list." << endl;
+                }
+                if (spaceCount < size - 1) {
+                    invalid = true;
+                    cout << endl << "There aren't enough numbers for the size of " << size << ", please re-enter the list." << endl;
+                }
+
+                string word;
+                int listInd = 0;
+
+                for (auto letter : strList) {
+                    if (letter == ' ') {
+                        arr[listInd] = stoi(word);
+                        word = "";
+                        listInd = listInd + 1;
+                    }
+                    else {
+                        word += letter;
+                    }
+                }
+            }
+        }
+
+        delete[] arr;
+    }
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
