7701d5d1b98573bb91a4d64d1405d55a9310aec5
JT Salisbury <crazyscouter1@gmail.com>
2020-03-04 12:25:17 -0500

    Comments

--- HW3/UserInterface.cpp
+++ HW3/UserInterface.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// Print all the valid options
 void UserInterface::printOptions() {
 	cout << endl << "1. Add a new person" << endl <<
 		"2. Delete a person" << endl <<
@@ -12,16 +13,18 @@ void UserInterface::printOptions() {
 		"6. Quit and save to a file" << endl;
 }
 
+// Is the option valid?
 bool UserInterface::validateOption(int option) {
 	return !(option < 1 || option > 6);
 }
 
+// Interface with the tree and execute a validated option
 void UserInterface::executeOption(Book* book, int option) {
 	string fname, lname, number, path;
 	vector<string> members;
 
 	switch (option) {
-		case 1: 
+		case 1: // Add a new member
 			cout << "Please enter the person's first name: ";
 			cin >> fname;
 
@@ -33,7 +36,7 @@ void UserInterface::executeOption(Book* book, int option) {
 
 			book->insert(fname, lname, number);
 			break;
-		case 2: 
+		case 2: // Delete a member
 			cout << "Please enter the person's first name: ";
 			cin >> fname;
 
@@ -42,7 +45,7 @@ void UserInterface::executeOption(Book* book, int option) {
 
 			book->remove(fname, lname);
 			break;
-		case 3: 
+		case 3:  // Find the number for a person
 			cout << "Please enter the person's first name: ";
 			cin >> fname;
 
@@ -52,7 +55,7 @@ void UserInterface::executeOption(Book* book, int option) {
 			number = book->findNumber(fname, lname);
 			cout << endl << fname << ' ' << lname << "'s number: " << number;
 			break;
-		case 4: 
+		case 4:  // Change a person's phone number
 			cout << "Please enter the person's first name: ";
 			cin >> fname;
 
@@ -64,14 +67,14 @@ void UserInterface::executeOption(Book* book, int option) {
 
 			book->change(fname, lname, number);
 			break;
-		case 5: 
+		case 5: // Get all the members of the phonebook and print them out in ascending order
 			members = book->getAllAscending();
 			cout << endl << "Phonebook Members:" << endl;
 			for (int i = 0; i < members.size(); i++) {
 				cout << members[i] << endl;
 			}
 			break;
-		case 6:
+		case 6: // Quit and save the tree to a file
 			cout << "Please enter the full path of where to save the tree: ";
 			cin >> path;
 
@@ -81,6 +84,7 @@ void UserInterface::executeOption(Book* book, int option) {
 	}
 }
 
+// Whether or not the UI should quit
 bool UserInterface::shouldQuit() {
 	return m_quit;
 }
--- HW3/enc_temp_folder/f2f77f6c5c78e2bca7a2314085d34285/HW3.cpp
+++ HW3/enc_temp_folder/f2f77f6c5c78e2bca7a2314085d34285/HW3.cpp
@@ -12,12 +12,10 @@ int main() {
 	int option = -1;
 	Book* book;
 
-	// Option to create the new tree based off a file or not
 	while (option != 1 && option != 2) {
 		cout << "Would you like to create a blank tree (1) or one based off a file (2)? ";
 		cin >> option;
-		
-		// In case cin resolves to a fail state
+
 		if (cin.fail()) {
 			cin.clear();
 			cin.ignore(numeric_limits<streamsize>::max(), '\n');
@@ -25,7 +23,6 @@ int main() {
 	}
 
 	if (option == 2) {
-		// Create the tree via a file
 		string filepath;
 
 		cout << "Please enter the full path to the tree file: ";
@@ -33,29 +30,24 @@ int main() {
 
 		book = new Book(filepath);
 	} else {
-		// Create the tree with nothing
 		book = new Book();
 	}
-	
-	// UI class will provide a quit state
+
 	while (!ui->shouldQuit()) {
 		option = -1;
 
-		// While the validation option is not valid
 		while (!ui->validateOption(option)) {
 			ui->printOptions();
 
 			cout << "Please enter an option: ";
 			cin >> option;
 
-			// Ensure the cin isn't in a fail state. If it is, fix it
 			if (cin.fail()) {
 				cin.clear();
 				cin.ignore(numeric_limits<streamsize>::max(), '\n');
 			}
 		}
 
-		// Execute an option 
 		ui->executeOption(book, option);
 	}
 }
